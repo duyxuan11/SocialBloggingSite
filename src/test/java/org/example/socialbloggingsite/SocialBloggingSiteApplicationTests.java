58df3b4d4383b0b2d9cdd77117bc0dbf69cc9d0a
duyxuan11 <xuanduy023@gmail.com>
2024-09-17 14:49:40 +0700

    First code

---
+++ src/test/java/org/example/socialbloggingsite/SocialBloggingSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.socialbloggingsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialBloggingSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
