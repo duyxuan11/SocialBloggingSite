096ace1d5cbc2311196f8e7ed21c09182d0cec51
duyxuan11 <xuanduy023@gmail.com>
2024-10-04 09:01:35 +0700

    comment cr-d

--- src/main/java/org/example/socialbloggingsite/comments/dto/ArticleCommentResponse.java
+++ src/main/java/org/example/socialbloggingsite/comments/dto/ArticleCommentResponse.java
@@ -1,23 +1,21 @@
-package org.example.socialbloggingsite.articles.dto;
+package org.example.socialbloggingsite.comments.dto;
 
 import lombok.*;
 import lombok.experimental.FieldDefaults;
-import org.example.socialbloggingsite.comments.dto.CommentArticleResponse;
+import org.example.socialbloggingsite.articles.dto.UserArticleResponse;
 
 import java.util.Date;
-import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class ArticleResponse {
+public class ArticleCommentResponse {
     int id;
     String title;
     String content;
     Date createdAt;
     int totalFavorites;
     UserArticleResponse user;
-    List<CommentArticleResponse> comments;
 }
